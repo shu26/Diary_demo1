88249645c977cf8f9014f01fecae936e0a0cebfa
海法修平 <fnmmbestsk15kkpo@gmail.com>
2019-06-20 17:37:13 +0900

    Initial Commit

---
+++ Diary_demo1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Diary_demo1
+//
+//  Created by 海法修平 on 2019/06/20.
+//  Copyright © 2019 海法修平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
